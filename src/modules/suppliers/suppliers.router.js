af9a08abb511afa754d637e7d2e73c6c8e68f456
StephenTuan <nguyenanhtuann204@gmail.com>
2025-11-14 17:09:14 +0700

    Feature/crud medicines: implement api medicines
    * them truong quantity cho medicines
    
    * update api suppliers
    
    * remove quantity

--- src/modules/suppliers/suppliers.router.js
+++ src/modules/suppliers/suppliers.router.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import SuppliersController from './suppliers.controller.js'
+import { validateBody } from '../../middlewares/validate.js'
+import { createSupplierSchema, updateSupplierSchema } from './suppliers.validation.js'
 
 const router = express.Router()
 
 router.get('/', SuppliersController.getAll)
 router.get('/:id', SuppliersController.getById)
-router.post('/', SuppliersController.create)
-router.put('/:id', SuppliersController.update)
+router.post('/', validateBody(createSupplierSchema), SuppliersController.create)
+router.put('/:id', validateBody(updateSupplierSchema), SuppliersController.update)
 router.delete('/:id', SuppliersController.delete)
 
 export default router
